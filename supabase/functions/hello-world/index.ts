4b02b292b17a61b8fcfea33e231b47fcf022e53b
gabrielmaia <gabrielmaia.web@gmail.com>
2026-01-29 21:47:14 -0300

    poc supabase with hono

---
+++ supabase/functions/hello-world/index.ts
@@ -0,0 +1,25 @@
+import { createClient } from '@supabase/supabase-js'
+import { Hono, Context } from 'jsr:@hono/hono'
+
+// change this to your function name
+const functionName = 'hello-world'
+const app = new Hono().basePath(`/${functionName}`)
+
+app.get('/hello', (c: Context) => c.text('Hello from hono-server!'))
+
+app.get('/products', async (c: Context) => {
+  const supabase = createClient(
+    Deno.env.get('SUPABASE_URL') ?? '',
+    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+  )
+
+  const { data, error } = await supabase.from('products').select('*')
+
+  if (error) {
+    return c.json({ error: error.message }, 500)
+  }
+
+  return c.json(data)
+})
+
+Deno.serve(app.fetch)
