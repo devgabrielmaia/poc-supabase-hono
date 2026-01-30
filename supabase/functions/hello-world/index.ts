0c9d63a7d83620e77fbb515c1b105102f7460276
gabrielmaia <gabrielmaia.web@gmail.com>
2026-01-29 21:48:27 -0300

    improve

--- supabase/functions/hello-world/index.ts
+++ supabase/functions/hello-world/index.ts
@@ -12,13 +12,10 @@ app.get('/products', async (c: Context) => {
     Deno.env.get('SUPABASE_URL') ?? '',
     Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
   )
-
   const { data, error } = await supabase.from('products').select('*')
-
   if (error) {
     return c.json({ error: error.message }, 500)
   }
-
   return c.json(data)
 })
 
